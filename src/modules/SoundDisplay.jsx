d24904d96b9edd580314538a60cb247ea089cb37
Oh Balaclava <ohbalaclava@disnae.org>
2025-09-11 01:29:56 +0100

    reduce padding in sound display

--- src/modules/SoundDisplay.jsx
+++ src/modules/SoundDisplay.jsx
@@ -8,7 +8,7 @@ export function SoundDisplay(initialVnode) {
             return (
                 <div className="overflow-hidden rounded-lg border border-gray-100 bg-white shadow-xs dark:border-gray-800 dark:bg-gray-900 dark:shadow-gray-700/25">
                     <DotNotation code={vnode.attrs.sound.dot}/>
-                    <div className="p-4 sm:p-6">
+                    <div className="p-1 sm:p-2">
                         <a href="#">
                             <h3 className="text-lg font-medium text-gray-900 dark:text-white">
                                 {vnode.attrs.sound.sound}
