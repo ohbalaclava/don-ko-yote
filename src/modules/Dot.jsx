5ec246ba0c68b6721d9812731af64b163f62194c
Oh Balaclava <ohbalaclava@disnae.org>
2025-09-12 01:27:41 +0100

    support more dot combos. changed ka to a horizontal line rather than a cross - it's cleaner particularly when also splitting the dot

--- src/modules/Dot.jsx
+++ src/modules/Dot.jsx
@@ -25,11 +25,35 @@ export function Dot(initialVnode) {
                                 <path d={"M " + x + " " + (y - r - 1) + " v " + (2 * r + 2)} className="dot_break"/>
                             </g>
                         );
+                    case "12":
+                        return (
+                            <g>
+                                <circle cx={x} cy={y} r={r} className="dot_full"/>
+                                <path d={"M " + x + " " + y + " h " + (r + 1)} className="dot_break"/>
+                                <path d={"M " + x + " " + (y - r - 1) + " v " + (2 * r + 2)} className="dot_break"/>
+                            </g>
+                        );
                     case "20":
                         return (
                             <g>
                                 <circle cx={x} cy={y} r={r} className="dot_full"/>
-                                <path d={crossPath(x, y, r)} className="dot_ka"/>
+                                <path d={"M " + (x - r - 1) + " " + y + " h " + (2 * r + 2)} className="dot_break"/>
+                            </g>
+                        );
+                    case "21":
+                        return (
+                            <g>
+                                <circle cx={x} cy={y} r={r} className="dot_full"/>
+                                <path d={"M " + (x - r - 1) + " " + y + " h " + (r + 1)} className="dot_break"/>
+                                <path d={"M " + x + " " + (y - r - 1) + " v " + (2 * r + 2)} className="dot_break"/>
+                            </g>
+                        );
+                    case "22":
+                        return (
+                            <g>
+                                <circle cx={x} cy={y} r={r} className="dot_full"/>
+                                <path d={"M " + (x - r - 1) + " " + y + " h " + (2 * r + 2)} className="dot_break"/>
+                                <path d={"M " + x + " " + (y - r - 1) + " v " + (2 * r + 2)} className="dot_break"/>
                             </g>
                         );
                     case "30":
