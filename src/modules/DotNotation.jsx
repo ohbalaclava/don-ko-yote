9e850159a5256ed9bd8c4e5246bb47e9130f1c0e
Oh Balaclava <ohbalaclava@disnae.org>
2025-09-12 01:25:41 +0100

    support for 2 & 3 meter

--- src/modules/DotNotation.jsx
+++ src/modules/DotNotation.jsx
@@ -2,6 +2,7 @@
 import m from "mithril";
 
 import {Dot} from "./Dot.jsx";
+import model from "../data/model.js";
 
 export function DotNotation(initialVnode) {
     function beat(code, division) {
@@ -9,15 +10,36 @@ export function DotNotation(initialVnode) {
         return code.substring(start, start + 2);
     }
 
+    function meter2Dots(code) {
+        return (
+            <svg viewBox="0 0 81 20" xmlns="http://www.w3.org/2000/svg">
+                <Dot x="27" y="10" r="5" beat={beat(code, 0)}/>
+                <Dot x="54" y="10" r="5" beat={beat(code, 1)}/>
+            </svg>
+        )
+    }
+
+    function meter3Dots(code) {
+        return (
+            <svg viewBox="0 0 80 20" xmlns="http://www.w3.org/2000/svg">
+                <Dot x="25" y="10" r="5" beat={beat(code, 0)}/>
+                <Dot x="40" y="10" r="5" beat={beat(code, 1)}/>
+                <Dot x="55" y="10" r="5" beat={beat(code, 2)}/>
+            </svg>
+        );
+    }
+
     return {
         view: (vnode) => {
-            return (
-                <svg viewBox="0 0 80 20" xmlns="http://www.w3.org/2000/svg">
-                    <Dot x="25" y="10" r="5" beat={beat(vnode.attrs.code, 0)}/>
-                    <Dot x="40" y="10" r="5" beat={beat(vnode.attrs.code, 1)}/>
-                    <Dot x="55" y="10" r="5" beat={beat(vnode.attrs.code, 2)}/>
-                </svg>
-            )
+            return (() => {
+                    switch (model.getCurrentJiuchi().meter) {
+                        case 2:
+                            return meter2Dots(vnode.attrs.code);
+                        case 3:
+                            return meter3Dots(vnode.attrs.code);
+                    }
+                }
+            )();
         }
     }
 }
